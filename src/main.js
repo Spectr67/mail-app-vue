d4d9b9bd4883b2f5032b48342012aebdc84cd8c7
Arthur <pennywennyweed@gmail.com>
2025-06-22 22:45:11 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css'
+import 'material-icons'
+
+createApp(App).mount('#app')
