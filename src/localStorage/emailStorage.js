15db9fef9a321bbbf08deac76c1fc44e07c36782
Arthur <pennywennyweed@gmail.com>
2025-07-05 11:26:22 +0300

    add local storage for emails

---
+++ src/localStorage/emailStorage.js
@@ -0,0 +1,15 @@
+const emails = []
+
+function setEmails(emails) {
+  localStorage.setItem('emails', JSON.stringify(emails))
+}
+
+function getEmails() {
+  const data = localStorage.getItem('emails')
+  return JSON.parse(data)
+}
+function addEmail(email) {
+  const emails = getEmails()
+  emails.push(email)
+  setEmails(emails)
+}
