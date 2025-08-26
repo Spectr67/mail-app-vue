d49f5e8e22a541d2e824c2bceb054ba62d9388df
Arthur <pennywennyweed@gmail.com>
2025-08-26 17:09:58 +0300

    add bootstrap customize components

---
+++ src/components/WrapForm.vue
@@ -0,0 +1,46 @@
+<script>
+import { BButton, BForm } from 'bootstrap-vue-next'
+import WrapFormInput from './WrapFormInput.vue'
+
+export default {
+  components: { BForm, BButton, WrapFormInput },
+  emits: ['submitted'],
+  props: ['skeleton', 'struct'],
+
+  data() {
+    return {
+      localStruct: JSON.parse(JSON.stringify(this.struct)),
+    }
+  },
+
+  methods: {
+    submit() {
+      let account = Object.fromEntries(
+        Object.entries(this.localStruct).map(([key, { data }]) => [key, data])
+      )
+      this.$emit('submitted', account)
+      this.localStruct = JSON.parse(JSON.stringify(this.struct))
+    },
+  },
+}
+</script>
+
+<template>
+  <div>
+    <div v-for="(row, idx) of skeleton" :key="idx" class="row">
+      <div v-for="(col, key) of row" :key="key" class="col" :class="col">
+        <WrapFormInput
+          :is="localStruct[key].component"
+          :caption="localStruct[key].caption"
+          v-model="localStruct[key].data"
+          :type="localStruct[key].type"
+          class="validate"
+          required
+        />
+      </div>
+    </div>
+    <BForm @submit.prevent="submit">
+      <BButton variant="success" type="submit">Submit</BButton>
+    </BForm>
+  </div>
+</template>
---
+++ src/components/WrapFormInput.vue
@@ -0,0 +1,20 @@
+<script>
+import { BFormFloatingLabel, BFormInput } from 'bootstrap-vue-next'
+
+export default {
+  components: { BFormFloatingLabel, BFormInput },
+  emits: ['update:modelValue'],
+  props: ['type', 'caption', 'modelValue'],
+}
+</script>
+
+<template>
+  <BFormFloatingLabel :label="caption" class="my-2">
+    <BFormInput
+      :model-value="modelValue"
+      @update:model-value="$emit('update:modelValue', $event)"
+      :type="type || 'text'"
+      :placeholder="caption"
+    />
+  </BFormFloatingLabel>
+</template>
